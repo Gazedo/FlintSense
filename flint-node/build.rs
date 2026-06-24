56e2831a29ca9e0b79583db72f5a907ca3afe3be
Hayden Connor <hconnor3@gmail.com>
2026-06-23 23:05:11 -0700

    Working copy of flint node

--- flint-node/build.rs
+++ flint-node/build.rs
@@ -2,4 +2,5 @@ fn main() {
     println!("cargo:rustc-link-search={}", env!("CARGO_MANIFEST_DIR"));
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+    println!("cargo:rerun-if-changed=memory.x");
 }
